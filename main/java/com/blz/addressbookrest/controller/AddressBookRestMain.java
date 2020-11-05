5e16d40f2553a7ba25b9d20ead261a69ada0322f
dev01997D <devnandan01997@gmail.com>
2020-11-05 16:51:56 +0530

    Retrieved: All the Contacts from the Rest API server is retrieved and verified by its count

---
+++ main/java/com/blz/addressbookrest/controller/AddressBookRestMain.java
@@ -0,0 +1,33 @@
+package com.blz.addressbookrest.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.blz.addressbookrest.model.Contact;
+
+public class AddressBookRestMain {
+	private List<Contact> contactList;
+
+	public enum IOService {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO;
+	}
+
+	public AddressBookRestMain() {
+	}
+
+	public AddressBookRestMain(List<Contact> contactList) {
+		this();
+		this.contactList = new ArrayList<>(contactList); // Use new memory not the same as provided by client to avoid confusion
+
+	}
+
+	public long countEntries(IOService ioService) {
+		if (ioService.equals(IOService.REST_IO))
+			return contactList.size();
+		return 0;
+	}
+
+	public void addContactToAddressBook(Contact contactData, IOService restIo) {
+		contactList.add(contactData);
+	}
+}
